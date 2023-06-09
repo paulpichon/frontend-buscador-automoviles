f215c707fc7ab4de7cef13015a879a9fa5b0b9c4
paul pichon <paul10_barca@hotmail.com>
2023-06-09 12:47:04 -0600

    Se cambio la ruta principal de la API de un ambiot local a uno en produccion

--- js/db.js
+++ js/db.js
@@ -5,7 +5,7 @@ const automoviles = async() => {
 		redirect: 'follow'
 	  };
 	  
-	const response = await fetch("http://localhost:5000/api/automoviles", requestOptions);
+	const response = await fetch("https://apiautomovil-production-0c64.up.railway.app/api/automoviles", requestOptions);
 	const resultado = await response.json();
 
 	if ( response.status === 200 ) {
